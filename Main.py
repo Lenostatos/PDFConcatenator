67e38b4caaeb3428bb384cddf985540a17f84d42
Lenostatos <Lenostatos@gmx.de>
2019-12-10 22:21:21 +0100

    Integrate the new header extraction into main.

--- Main.py
+++ Main.py
@@ -7,19 +7,19 @@ import os
 
 import PyPDF2
 
-import HelperFunctions
+import HelperClasses
 
 # Path to directory that contains the PDF documents with our answers
 working_directory = pathlib.Path(
     'd:/Studium_ForestInformationTechnology/Scientific_Internet_Colloquium/'
-    'Assignment_2/colleagues_evaluations/group_1'
+    'Assignment_3/colleagues_evaluations/'
 )
 
 if not working_directory.exists():
     print("Error: The folder with the PDF documents could not be found!")
     exit(code=1)
 
-output_file_name = 'all_evaluations.pdf'
+output_file_name = 'SIC_03_all_answers.pdf'
 output_file_path = working_directory / output_file_name
 
 # Delete an existing PDF collection with the same name
@@ -35,7 +35,7 @@ my_PDF_merger = PyPDF2.PdfFileMerger()
 for file_path_obj in pdf_file_paths:
     my_PDF_merger.append(
         str(file_path_obj),
-        bookmark=HelperFunctions.extract_student_name_from(file_path_obj)
+        bookmark=HelperClasses.SICFileName(file_path_obj).extract_header()
     )
 
 # Write the collection PDF to a file
